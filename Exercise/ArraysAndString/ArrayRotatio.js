783e93484c8e8e5e5ccccc6f6fcbf776c431bf9f
daniel-tsonkov <daniel_tzonkov@abv.bg>
2023-11-04 09:45:34 +0000

    up

--- Exercise/ArraysAndString/ArrayRotatio.js
+++ Exercise/ArraysAndString/ArrayRotatio.js
@@ -1,5 +1,10 @@
-function solve() {
-console.log("test");
-}
+function solve(arr, rotations) {
+    for (let i = 0; i < rotations; i++) {
+        let value = arr.shift();
+    
+    arr.push(value);
+    }
+    console.log(arr.join(' '));
+    }
 
 solve([51, 47, 32, 61, 21], 2);
