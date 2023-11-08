208bf9436ef850a6e4879787811dbdde5bc18065
daniel-tsonkov <daniel_tzonkov@abv.bg>
2023-11-08 12:20:29 +0000

    up

--- Exercise/FunctionsАndStatements/2. AddАndSubtract.js
+++ Exercise/FunctionsАndStatements/2. AddАndSubtract.js
@@ -1,14 +1,16 @@
-function sum(num1, num2) {
-    return num1 + num2;
-}
 
-function subtract(num1, num2) {
-    return num1 - num2;
-}
 
 function solve(num1, num2, num3) {
     let sumOperation = sum(num1, num2);
     let result = subtract(sumOperation, num3);
+
+    function sum(num1, num2) {
+        return num1 + num2;
+    }
+    
+    function subtract(num1, num2) {
+        return num1 - num2;
+    }
 
     return result;
 }
