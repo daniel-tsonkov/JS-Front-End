a8973aa5e0d4c3ce689cecf3fd5d57ab8ac803d3
daniel-tsonkov <daniel_tzonkov@abv.bg>
2023-11-08 12:54:08 +0000

    up

---
+++ Exercise/FunctionsАndStatements/LoadingBar.js
@@ -0,0 +1,11 @@
+function solve(num) {
+    let percantage = num / 10;
+    let leftPercantage = 10 - percantage;
+    if(leftPercantage !== 0) {
+        console.log(`${num}% [${'%'.repeat(percantage)}${'.'.repeat(leftPercantage)}]\nStill loading...`)
+    } else {
+        console.log('100% Complete!')
+    }
+}
+
+solve(30);
