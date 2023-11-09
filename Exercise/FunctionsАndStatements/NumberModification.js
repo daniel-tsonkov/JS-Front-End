2c7ae98a72688b59282ae5c7952614c4b8dbe007
daniel-tsonkov <daniel_tzonkov@abv.bg>
2023-11-09 12:36:52 +0000

    up

---
+++ Exercise/FunctionsАndStatements/NumberModification.js
@@ -0,0 +1,19 @@
+function solve(number) {
+    num = number.toString();
+    let average = num
+        .split('')
+        .map(Number)
+        .reduce((total, curr) => total + curr, 0) / num.length;
+
+    while (average < 5) {
+        num += '9';
+        average = num
+            .split('')
+            .map(Number)
+            .reduce((total, curr) => total + curr, 0) / num.length;
+    }
+
+    console.log(num);
+}
+
+solve(5835);
