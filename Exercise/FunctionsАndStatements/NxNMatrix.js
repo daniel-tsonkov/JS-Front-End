3f06c6f65ad056b9d5fdfc6c7abb62a4bd33b274
daniel-tsonkov <daniel_tzonkov@abv.bg>
2023-11-13 14:20:38 +0000

    up

--- Exercise/FunctionsАndStatements/NxNMatrix.js
+++ Exercise/FunctionsАndStatements/NxNMatrix.js
@@ -1,5 +1,12 @@
 function solve(num) {
+    function repearChar(num) {
+        let numtoChar = num + ' ';
+        console.log(numtoChar.repeat(num).trimEnd());
+    }
 
+    for (let i = 0; i < num; i++) {
+        repearChar(num);
+    }
 }
 
 solve(7);
