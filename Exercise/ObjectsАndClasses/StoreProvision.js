2442349ae3eef9824ae58a6236f141ec639bf60e
daniel-tsonkov <daniel_tzonkov@abv.bg>
2023-11-15 14:37:15 +0000

    up

---
+++ Exercise/ObjectsАndClasses/StoreProvision.js
@@ -0,0 +1,8 @@
+function solve(currentStock, deliveryStock) {
+    const products = [...currentStock, ...deliveryStock];
+}
+
+solve(
+    ['Chips', '5', 'CocaCola', '9', 'Bananas', '14', 'Pasta', '4', 'Beer', '2'],
+    ['Flour', '44', 'Oil', '12', 'Pasta', '7', 'Tomatoes', '70', 'Bananas', '30']
+)
