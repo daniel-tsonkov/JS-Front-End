ba66d4707c08e0062b66a44b37c3d68776f19ff2
daniel-tsonkov <daniel_tzonkov@abv.bg>
2023-11-15 14:17:24 +0000

    up

--- Exercise/ObjectsАndClasses/Towns.js
+++ Exercise/ObjectsАndClasses/Towns.js
@@ -1,12 +1,10 @@
 function solve(input) {
     const cityes = input.map((city) => {
         const [name, lat, lon] = city.split(' | ');
-        return { town: name, latitude: lat, longitude: lon };
+        return { town: name, latitude: Number(lat), longitude: Number(lon) };
     }).forEach(element => {
-        console.log(`town: , latitude: ${Element.lat}, longitude: ${Element.lon}`);
+        console.log(`{ town: '${element.town}', latitude: '${element.latitude.toFixed(2)}', longitude: '${element.longitude.toFixed(2)}' }`);
     });;
-
-    
 }
 
 solve(['Sofia | 42.696552 | 23.32601',
