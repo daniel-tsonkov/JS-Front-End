c76a5feb9af407e1b60ec21dd41d10854a043c35
daniel-tsonkov <daniel_tzonkov@abv.bg>
2023-11-17 15:26:56 +0000

    up

--- Exercise/ObjectsАndClasses/Movies.js
+++ Exercise/ObjectsАndClasses/Movies.js
@@ -1,13 +1,32 @@
 function solve(input) {
+    let movies = [];
+
     input.forEach(command => {
-if (command.includes("addMovie")) {
-    //console.log(command);
-}else if (command.includes("directedBy")) {
-    //console.log(command);
-}else if (command.includes("onDate")) {
-    console.log(command);
-};
+        if (command.includes("addMovie")) {
+            //console.log(command.replace('addMovie ', ''));
+            const name = command.replace('addMovie ', '');
+            movies.push({
+                name
+            })
+        } else if (command.includes("onDate")) {
+            //console.log(command);
+            const [movieName, date] = command.split(' onDate ');
+            const movie = movies.find(m => m.name === movieName);
+
+            if (movie) {
+                movie.date = date;
+            }
+        } else if (command.includes("directedBy")) {
+            //console.log(command.replace(' directedBy ', ' '));
+            const [movieName, directorName] = command.split(' directedBy ');
+            const movie = movies.find(m => m.name === movieName);
+
+            if (movie) {
+                movie.director = directorName;
+            }
+        };
     });
+    console.log(JSON.stringify(movies));
 }
 
 solve([
