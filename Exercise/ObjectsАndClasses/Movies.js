cf2bd20bd14e3ab94d747ecf6b62854e6e843f6a
daniel-tsonkov <daniel_tzonkov@abv.bg>
2023-11-18 17:53:21 +0000

    up

--- Exercise/ObjectsАndClasses/Movies.js
+++ Exercise/ObjectsАndClasses/Movies.js
@@ -1,32 +1,36 @@
 function solve(input) {
     let movies = [];
 
+    const movieLibrary = {
+        addMovie: (command) => {
+            const [_, name] = command.split('addMovie ');
+            movies.push({ name });
+        },
+    }
+
     input.forEach(command => {
         if (command.includes("addMovie")) {
-            //console.log(command.replace('addMovie ', ''));
-            const name = command.replace('addMovie ', '');
-            movies.push({
-                name
-            })
+            movieLibrary.addMovie(command);
         } else if (command.includes("onDate")) {
-            //console.log(command);
             const [movieName, date] = command.split(' onDate ');
-            const movie = movies.find(m => m.name === movieName);
+            const movie = movies.find((m) => m.name === movieName);
 
             if (movie) {
                 movie.date = date;
             }
         } else if (command.includes("directedBy")) {
-            //console.log(command.replace(' directedBy ', ' '));
             const [movieName, directorName] = command.split(' directedBy ');
-            const movie = movies.find(m => m.name === movieName);
+            const movie = movies.find((m) => m.name === movieName);
 
             if (movie) {
                 movie.director = directorName;
             }
         };
     });
-    console.log(JSON.stringify(movies));
+    //console.log(JSON.stringify(movies));
+    movies
+    .filter((m) => m.name && m.director && m.date)
+    .forEach((m) => console.log(JSON.stringify(m)));
 }
 
 solve([
