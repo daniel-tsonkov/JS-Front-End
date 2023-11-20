3dcbfa650ab0200ad687840e5f37a777915824cc
daniel-tsonkov <daniel_tzonkov@abv.bg>
2023-11-20 08:46:17 +0000

    up

--- Exercise/ObjectsАndClasses/Inventory.js
+++ Exercise/ObjectsАndClasses/Inventory.js
@@ -1,10 +1,17 @@
-function solve() {
-
+function solve(input) {
+    let heroes = [];
+    for (const heroData of heroes) {
+        let [name, level, items] = heroData.split(" - ");
+        let hero = {
+            name: name,
+            level: Number(level),
+            item: items
+        }
+    }
 }
 
 solve([
     'Isacc / 25 / Apple, GravityGun',
     'Derek / 12 / BarrelVest, DestructionSword',
     'Hes / 1 / Desolator, Sentinel, Antara'
-    ]
-    );
+]);
