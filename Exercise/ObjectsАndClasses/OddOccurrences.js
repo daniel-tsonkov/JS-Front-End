a8cffff3c38d87f09138b36e511a759304520f05
daniel-tsonkov <daniel_tzonkov@abv.bg>
2023-11-21 14:16:21 +0000

    up

--- Exercise/ObjectsАndClasses/OddOccurrences.js
+++ Exercise/ObjectsАndClasses/OddOccurrences.js
@@ -1,5 +1,21 @@
-function solve() {
+function solve(input) {
+    input = input.toLowerCase();
+    let arrayElements = input.split(' ');
 
+    let map = new Map();
+
+    arrayElements.forEach(element => {
+        if (map.has(element)) {
+            let oldValue = map.get(element);
+            let newValue = oldValue + 1;
+            map.set(element, newValue);
+        } else {
+            map.set(element, 1);
+        }
+
+        
+    });
+    console.log(map);
 }
 
 solve('Java C# Php PHP Java PhP 3 C# 3 1 5 C#');
