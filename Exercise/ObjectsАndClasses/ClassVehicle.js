f972fc189abd6acbc6906c75b838f28524b247a8
daniel-tsonkov <daniel_tzonkov@abv.bg>
2023-11-22 19:00:35 +0000

    up

--- Exercise/ObjectsАndClasses/ClassVehicle.js
+++ Exercise/ObjectsАndClasses/ClassVehicle.js
@@ -1,5 +1,22 @@
-function solve() {
+class Vehicle {
+    constructor(type, model, parts, fuel) {
+        this.type = type;
+        this.model = model;
+        this.parts = {
+            engine: parts.engine,
+            power: parts.power,
+            quality: parts.engine * parts.power
+        }
+        this.fuel = fuel;
+    }
 
+    drive(fuelLose) {
+        this.fuel -= fuelLose;
+    }
 }
 
-solve();
+let parts = { engine: 6, power: 100 };
+let vehicle = new Vehicle('a', 'b', parts, 200);
+vehicle.drive(100);
+console.log(vehicle.fuel);
+console.log(vehicle.parts.quality);
