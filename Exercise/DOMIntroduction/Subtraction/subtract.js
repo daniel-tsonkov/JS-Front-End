b577c2ded11654cf58bc34bcc493306c98f2adfe
daniel-tsonkov <daniel_tzonkov@abv.bg>
2023-11-25 19:02:07 +0200

    up

--- Exercise/DOMIntroduction/Subtraction/subtract.js
+++ Exercise/DOMIntroduction/Subtraction/subtract.js
@@ -1,5 +1,6 @@
 function subtract() {
     const firstNumber = Number(document.getElementById('firstNumber').value);
-
-    console.log(firstNumber);
+    const secondNumber = Number(document.getElementById('secondNumber').value);
+    let result = document.getElementById('result');
+    result.innerText = firstNumber - secondNumber;
 }
