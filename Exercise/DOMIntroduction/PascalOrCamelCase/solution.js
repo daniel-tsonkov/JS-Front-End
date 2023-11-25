375cb487b7ec0de22c045e980a0c0e38dca1a18c
daniel-tsonkov <daniel_tzonkov@abv.bg>
2023-11-25 20:03:10 +0200

    up

--- Exercise/DOMIntroduction/PascalOrCamelCase/solution.js
+++ Exercise/DOMIntroduction/PascalOrCamelCase/solution.js
@@ -1,16 +1,32 @@
 function solve() {
-  const text = document.querySelector('#text').value;
-  const convention = document.querySelector('#naming-convention').value;
+  let text = document.querySelector('#text').value;
+  let convention = document.querySelector('#naming-convention').value;
   let result = document.querySelector('#result');
 
   if (convention === 'Camel Case') {
     let sentence = [];
 
     text = text.split(' ');
-    sentence.push(text[0]);
+    sentence.push(text[0].toLowerCase());
 
-    for(let i = 1; i < text.lengthh)
+    for (let i = 1; i < text.length; i++) {
+      text[i] = text[i].toLowerCase();
+      sentence.push(text[i].charAt(0).toUpperCase() + text[i].slice(1));
+    }
+
+    result.textContent = sentence.join('');
+  } else if (convention === 'Pascal Case') {
+    let sentence = [];
+
+    text = text.split(' ');
+
+    for (let i = 0; i < text.length; i++) {
+      text[i] = text[i].toLowerCase();
+      sentence.push(text[i].charAt(0).toUpperCase() + text[i].slice(1));
+    }
+
+    result.textContent = sentence.join('');
+  } else {
+    result.textContent = 'Error!';
   }
-
-  console.log(text);
 }
