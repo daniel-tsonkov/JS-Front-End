8795e4d4c3c7c3c48ef53c728424b5ffecfdcf8d
daniel-tsonkov <daniel_tzonkov@abv.bg>
2023-11-25 19:39:44 +0200

    up

--- Exercise/DOMIntroduction/PascalOrCamelCase/solution.js
+++ Exercise/DOMIntroduction/PascalOrCamelCase/solution.js
@@ -1,3 +1,16 @@
 function solve() {
-  //TODO...
+  const text = document.querySelector('#text').value;
+  const convention = document.querySelector('#naming-convention').value;
+  let result = document.querySelector('#result');
+
+  if (convention === 'Camel Case') {
+    let sentence = [];
+
+    text = text.split(' ');
+    sentence.push(text[0]);
+
+    for(let i = 1; i < text.lengthh)
+  }
+
+  console.log(text);
 }
