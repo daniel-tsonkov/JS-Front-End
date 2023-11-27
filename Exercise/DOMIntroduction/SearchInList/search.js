c2505635ebfda663576f3418b8a79332199c274f
dan <daniel_tzonkov@abv.bg>
2023-11-27 13:54:17 +0200

    up

--- Exercise/DOMIntroduction/SearchInList/search.js
+++ Exercise/DOMIntroduction/SearchInList/search.js
@@ -1,3 +1,15 @@
 function search() {
-   // TODO
+   let items = Array.from(document.querySelectorAll('li'));
+   let myText = document.getElementById('searchText').value;
+   let result = document.getElementById('result');
+   let matches = 0;
+
+   for (const liItem of items) {
+      if (liItem.textContent.includes(myText)) {
+         matches++;
+         liItem.style.fontWeight = 'bold';
+         liItem.style.textDecoration = 'underline';
+      }
+   }
+   result.textContent = `${matches} matches found`;
 }
