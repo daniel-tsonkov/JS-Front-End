a15765af78a8ceb8b40c96b0a5f144502d5d4602
dan <daniel_tzonkov@abv.bg>
2023-11-27 14:16:04 +0200

    up

--- Exercise/DOMIntroduction/Table-SearchEngine/solution.js
+++ Exercise/DOMIntroduction/Table-SearchEngine/solution.js
@@ -1,5 +1,7 @@
 function solve() {
    document.querySelector('#searchBtn').addEventListener('click', onClick);
+   let input = document.getElementById('searchField');
+   let tableRowls = document.querySelectorAll('tbody tr');
 
    function onClick() {
       //   TODO:
