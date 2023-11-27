c674a9c4fc30c244177bdb7d838764fca8c88835
dan <daniel_tzonkov@abv.bg>
2023-11-27 14:31:05 +0200

    up

--- Exercise/DOMIntroduction/Table-SearchEngine/solution.js
+++ Exercise/DOMIntroduction/Table-SearchEngine/solution.js
@@ -8,12 +8,12 @@ function solve() {
          row.className = '';
       }
       for (const row of tableRowls) {
-         
+         row.classList.remove('select'); //Drug na`in za zachistwane na selekciata
          if (input.value !== '' && row.innerHTML.includes(input.value)) {
             console.log('asd');
             row.className = 'select';
          }
       }
-
+      input.value = '';
    }
 }
