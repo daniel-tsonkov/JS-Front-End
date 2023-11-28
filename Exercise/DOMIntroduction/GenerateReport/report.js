076d3491e140d590b882349ad3c761967bc5a27f
dan <daniel_tzonkov@abv.bg>
2023-11-28 15:17:36 +0200

    up

--- Exercise/DOMIntroduction/GenerateReport/report.js
+++ Exercise/DOMIntroduction/GenerateReport/report.js
@@ -1,3 +1,12 @@
 function generateReport() {
-    //TODO
+    let personObject = {};
+    let allPersonsObjects = [];
+    let columnSetNumbers = [];
+
+    let columsAll = document.querySelectorAll('thead tr th input');
+
+    for (const index of columsAll) {
+        let checkBox = index.checked;
+        console.log(checkBox);
+    }
 }
