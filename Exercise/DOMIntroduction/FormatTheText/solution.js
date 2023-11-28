12d63e3cb4a7f14bf645a3a30b15dc7fef5f49c4
dan <daniel_tzonkov@abv.bg>
2023-11-28 14:11:23 +0200

    up

--- Exercise/DOMIntroduction/FormatTheText/solution.js
+++ Exercise/DOMIntroduction/FormatTheText/solution.js
@@ -1,3 +1,8 @@
 function solve() {
-  //TODO
+  let rowText = document.getElementById('input').value;
+  let outputElement = document.getElementById('output');
+  let sentences = rowText.split('.');
+  sentences = sentences.filter(s => s.length > 0);
+
+  console.log(sentences);
 }
