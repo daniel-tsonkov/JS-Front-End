ae5cb41d90afc249ff26acf1f5798189c459053c
dan <daniel_tzonkov@abv.bg>
2023-11-28 15:56:29 +0200

    up

--- Exercise/DOMIntroduction/GenerateReport/report.js
+++ Exercise/DOMIntroduction/GenerateReport/report.js
@@ -5,8 +5,26 @@ function generateReport() {
 
     let columsAll = document.querySelectorAll('thead tr th input');
 
-    for (const index of columsAll) {
-        let checkBox = index.checked;
-        console.log(checkBox);
+    for (let index = 0; index < columsAll.length; index++) {
+        let checkBox = columsAll[index].checked;
+
+        if (checkBox) {
+            columnSetNumbers.push(index);
+        }
     }
+
+    let columnTitles = document.querySelectorAll('thead tr')[0].getElementsByTagName('th');
+    let rowCount = document.querySelectorAll('tbody tr').length;
+
+    for (let row = 0; row < rowCount; row++) {
+        columnSetNumbers.forEach(e => {
+            let key = columnTitles[e].textContent.trim().toLowerCase();
+            let value = document.querySelectorAll('tbody tr')[row].getElementsByTagName('td')[e].textContent;
+            personObject[key] = value;
+        });
+
+        allPersonsObjects.push(Object.assign(personObject));
+        personObject = {};
+    }
+    document.getElementById('output').innerHTML = JSON.stringify(allPersonsObjects);
 }
