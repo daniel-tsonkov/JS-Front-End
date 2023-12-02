519095728b5becaccb642b10f793d0ed8c9842f5
daniel-tsonkov <daniel_tzonkov@abv.bg>
2023-12-02 19:02:32 +0200

    up

--- Exercise/DOMАndEvents/Sections/script.js
+++ Exercise/DOMАndEvents/Sections/script.js
@@ -1,3 +1,17 @@
 function create(words) {
-   console.log('TODO:...');
+   const container = document.querySelector('#content');
+
+   words.forEach(word => {
+      const div = document.createElement('div');
+      const p = document.createElement('p');
+      p.textContent = word;
+      p.style.display = 'none';
+
+      div.append(p);
+      div.addEventListener('click', () => {
+         p.style.display = 'block';
+      });
+      container.appendChild(div);
+   });
+
 }
