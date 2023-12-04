194b23eecbe9ae310fe6a1f757a92ee04192e736
daniel-tsonkov <daniel_tzonkov@abv.bg>
2023-12-04 20:31:45 +0200

    up

--- Exercise/DOMАndEvents/Distance-Converter/distanceConverter.js
+++ Exercise/DOMАndEvents/Distance-Converter/distanceConverter.js
@@ -1,3 +1,26 @@
 function attachEventsListeners() {
-    console.log('TODO:...');
+    let [inputDistanceFiel, convertButton, outputDistanceFile] = document.getElementsByTagName('input');
+
+    let [inputUnitOption, outputUnitOption] = document.getElementsByTagName('option');
+
+    let convertionRates = {
+        'km': 1000,
+        'm': 1,
+        'cm': 0.01,
+        'mm': 0.001,
+        'mi': 1609.34,
+        'yrd': 0.9144,
+        'ft': 0.3048,
+        'in': 0.0254
+    }
+
+    convertButton.addEventListener('click', () => {
+        let inputDistance = inputDistanceFiel.value;
+        let inputUnits = inputUnitOption.value;
+        let outputUnits = outputUnitOption.value;
+
+        let result = inputDistance * convertionRates[inputUnits] / convertionRates[outputUnits];
+
+        outputDistanceFile.value = result;
+    });
 }
