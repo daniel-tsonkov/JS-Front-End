1bfdc7df624df1c7c2f4cbce9ed74815b50da074
daniel-tsonkov <daniel_tzonkov@abv.bg>
2023-12-04 19:46:59 +0200

    up

--- Exercise/DOMАndEvents/Encode-and-Decode-Messages/app.js
+++ Exercise/DOMАndEvents/Encode-and-Decode-Messages/app.js
@@ -1,3 +1,36 @@
 function encodeAndDecodeMessages() {
-    let encode = document.getElementsByTagName('textArea')[0];
+    let encodeTextArea = document.getElementsByTagName('textArea')[0];
+    let encodeBtn = document.getElementsByTagName('button')[0];
+
+    let decodeTextArea = document.getElementsByTagName('textArea')[1];
+    let decodeBtn = document.getElementsByTagName('button')[1];
+
+    encodeBtn.addEventListener('click', encodeText);
+    decodeBtn.addEventListener('click', decodeText);
+
+    function encodeText() {
+        let message = encodeTextArea.value;
+        let newMessage = '';
+
+        for (let index = 0; index < message.length; index++) {
+            let myChar = String.fromCodePoint(message[index].charCodeAt(0)+1);
+            newMessage += myChar;
+        }
+
+        decodeTextArea.value = newMessage;
+        encodeTextArea.value = '';
+    }
+
+    function decodeText() {
+        let message = decodeTextArea.value;
+        let newMessage = '';
+
+        for (let index = 0; index < message.length; index++) {
+            let myChar = String.fromCodePoint(message[index].charCodeAt(0)-1);
+            newMessage += myChar;
+        }
+
+        encodeTextArea.value = newMessage;
+        decodeTextArea.value = '';
+    }
 }
