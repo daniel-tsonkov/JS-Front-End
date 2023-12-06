75fdf667bc39a1fe4b60a091f7e60999088559ec
dan <daniel_tzonkov@abv.bg>
2023-12-06 11:53:59 +0200

    up

--- Exercise/DOMАndEvents/Furniture/solution.js
+++ Exercise/DOMАndEvents/Furniture/solution.js
@@ -1,15 +1,24 @@
 function solve() {
     const [generateTextAreas, buyTextArea] = Array.from(document.getElementsByTagName('textarea'));
     const [generateBtn, buyBtn] = Array.from(documents.getElementsByTagName('button'));
+    const tbody = document.querySelector('.table > tbody');
 
     generateBtn.addEventListener('click', generateHandler);
 
     function generateHandler() {
         const data = JSON.parse(generateTextAreas.value);
+        console.log('asd');
+
+        for (const { img, name, price, decFactor } of data) {
+            const tableRow = createElement('tr', '', tbody);
+            const firstColumnTd = createElement('td', '', tableRow);
+            const htmlImg = createElement('img', '', firstColumnTd, '', '', { src: img });
+            const secondColumnTd = createElement('td', '', tableRow);
+        }
     }
 }
 
-function createElement(type, content, id, classes, parentNode, attributes) {
+function createElement(type, content, parentNode, id, classes, attributes) {
     const htmlElement = document.createElement(type);
 
     if (content && type !== 'input') {
