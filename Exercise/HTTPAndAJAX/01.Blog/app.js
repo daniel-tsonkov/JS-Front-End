9e26fedf13ac279602074cff32672ae97d241463
dan <daniel_tzonkov@abv.bg>
2023-12-13 11:19:07 +0200

    up

--- Exercise/HTTPAndAJAX/01.Blog/app.js
+++ Exercise/HTTPAndAJAX/01.Blog/app.js
@@ -1,8 +1,31 @@
 function attachEvents() {
     const baseURL = 'http://localhost:3030/jsonstore/blog/';
+
     const loadPostsButton = document.getElementById('btnLoadPosts');
     const vewPosts = document.getElementById('btnViewPost');
-    const postSelection = document.getElementById('posts');
+    const postSelect = document.getElementById('posts');
+    const postTitle = document.getElementById('post-title');
+    const postBody = document.getElementById('post-body');
+    const postComments = document.getElementById('post-comments');
+
+    let allPosts = {};
+
+    loadPostsButton.addEventListener('click', async () => {
+
+        postSelect.innerHTML = '';
+
+        const response = await fetch(baseURL + 'posts');
+        allPosts = await response.json();
+
+        //console.log(allPosts);
+
+        for (const [postId, postObject] of Object.entries(allPosts)) {
+            const option = document.createElement('option');
+            option.value = postId;
+            option.textContent = postObject.title;
+            postSelect.appendChild(option);
+        }
+    })
 }
 
 attachEvents();
