c16817bdd99aec63e91598c1940b9d227188bbc9
dan <daniel_tzonkov@abv.bg>
2023-12-13 11:56:23 +0200

    up

--- Exercise/HTTPAndAJAX/01.Blog/app.js
+++ Exercise/HTTPAndAJAX/01.Blog/app.js
@@ -2,7 +2,7 @@ function attachEvents() {
     const baseURL = 'http://localhost:3030/jsonstore/blog/';
 
     const loadPostsButton = document.getElementById('btnLoadPosts');
-    const vewPosts = document.getElementById('btnViewPost');
+    const viewPosts = document.getElementById('btnViewPost');
     const postSelect = document.getElementById('posts');
     const postTitle = document.getElementById('post-title');
     const postBody = document.getElementById('post-body');
@@ -25,7 +25,29 @@ function attachEvents() {
             option.textContent = postObject.title;
             postSelect.appendChild(option);
         }
-    })
+    });
+
+    viewPosts.addEventListener('click', async () => {
+        postBody.innerHTML = '';
+        postComments.innerHTML = '';
+        
+        const postId = postSelect.value;
+
+        postTitle.textContent = allPosts[postId].title;
+        postBody.textContent = allPosts[postId].body;
+
+        const response = await fetch(baseURL + 'comments');
+        const commentsInfo = await response.json();
+
+        const filteredComments = Object.values(commentsInfo).filter(e => e.postId === postId);
+        filteredComments.forEach(element => {
+            const li = document.createElement('li');
+            li.id = element.id;
+            li.textContent = element.text;
+
+            postComments.appendChild(li);
+        });
+    });
 }
 
 attachEvents();
