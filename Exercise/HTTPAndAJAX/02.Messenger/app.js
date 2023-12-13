61142c5f80b96ea5ba1f236dd29267be75260a3a
dan <daniel_tzonkov@abv.bg>
2023-12-13 14:41:15 +0200

    up

--- Exercise/HTTPAndAJAX/02.Messenger/app.js
+++ Exercise/HTTPAndAJAX/02.Messenger/app.js
@@ -1,8 +1,32 @@
 function attachEvents() {
-    const baceURL = 'http://localhost3030/jsonstore/messenger';
+    const baseURL = 'http://localhost:3030/jsonstore/messenger';
 
     const [nameInput, contentInput, sendButton, refreshButton] = document.getElementsByTagName('input');
     const messagesTextArea = document.getElementById('messages');
+
+    sendButton.addEventListener('click', async () => {
+        const messageFormat = {
+            author: nameInput.value,
+            content: contentInput.value
+        };
+
+        if ((nameInput.value !== '') && (contentInput.value !== '')) {
+            await fetch(baseURL, {
+                method: 'POST',
+                body: JSON.stringify(messageFormat)
+            });
+        }
+
+        nameInput.value = '';
+        contentInput.value = '';
+    });
+
+    refreshButton.addEventListener('click', async () => {
+        const response = await fetch(baseURL)
+        const messages = await response.json();
+
+        console.log(messages);
+    })
 }
 
 attachEvents();
