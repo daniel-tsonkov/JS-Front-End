50dd53c01a8d290fd30bdbd9750e0e355d6a0eb0
dan <daniel_tzonkov@abv.bg>
2023-12-13 14:11:51 +0200

    up

--- Exercise/HTTPAndAJAX/02.Messenger/app.js
+++ Exercise/HTTPAndAJAX/02.Messenger/app.js
@@ -1,5 +1,8 @@
 function attachEvents() {
-    
+    const baceURL = 'http://localhost3030/jsonstore/messenger';
+
+    const [nameInput, contentInput, sendButton, refreshButton] = document.getElementsByTagName('input');
+    const messagesTextArea = document.getElementById('messages');
 }
 
 attachEvents();
