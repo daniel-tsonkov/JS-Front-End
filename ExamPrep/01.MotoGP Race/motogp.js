af7d932946e43ec7c1f425e582f184232e7786d0
dan <daniel_tzonkov@abv.bg>
2023-12-14 10:58:43 +0200

    up

--- ExamPrep/01.MotoGP Race/motogp.js
+++ ExamPrep/01.MotoGP Race/motogp.js
@@ -1,1 +1,24 @@
-//TODO...
+function solve(input) {
+    const riderCount = Number(input.shift()); //така се взима една линия запис
+    const riders = {};
+
+    for (let i = 0; i < riderCount.length; i++) {
+        const riderLine = input.shift();
+        //const riderDetails = riderLine.split('|');
+
+        //const name = riderDetails[0];
+        //const fuel = riderDetails[1];
+        //const position = riderDetails[2];
+
+        const [name, fuel, position] = riderLine.split('|');
+    }
+}
+
+solve(["3",
+    "Valentino Rossi|100|1",
+    "Marc Marquez|90|2",
+    "Jorge Lorenzo|80|3",
+    "StopForFuel - Valentino Rossi - 50 - 1",
+    "Overtaking - Marc Marquez - Jorge Lorenzo",
+    "EngineFail - Marc Marquez - 10",
+    "Finish"]);
